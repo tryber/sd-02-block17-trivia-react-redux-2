db89488ceeb577b09e0fda3e34ac156b7554d1a9
EddyeBoy27 <lipe0503@gmail.com>
2020-03-24 17:57:37 -0300

    CC Issues FIX

--- src/reducers/loadReducer.js
+++ src/reducers/loadReducer.js
@@ -3,6 +3,10 @@ import initialState from './initalState';
 
 export default function reduce(state = initialState, action) {
   switch (action.type) {
+    case types.LOAD_API:
+      return state;
+    case types.LOAD_USER:
+    return state;
     default:
       return state;
   }

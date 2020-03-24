de0ae4f72f41c7602a207228f8c3f07f2a77b7c4
EddyeBoy27 <lipe0503@gmail.com>
2020-03-24 17:55:11 -0300

    Criado pastas para o fluxo do redux

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,1 @@
+export const LOAD_API = 'LOAD_API';
---
+++ src/reducers/loadReducer.js
@@ -0,0 +1,9 @@
+import * as types from '../actions/actionTypes';
+import initialState from './initalState';
+
+export default function reduce(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}

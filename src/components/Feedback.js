211e78664c6b1b2f99109adc2e579653c5637471
miguel-miranda <ayandram7727@gmail.com>
2020-03-31 05:37:00 -0300

    CC

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -18,44 +18,64 @@ class Feedback extends Component {
     this.setState({ shouldRedirectToHome: true });
   }
 
-  render() {
-    const { gravatarEmail, name, score, assertions } = this.props;
-    const { shouldRedirectToRanking, shouldRedirectToHome } = this.state;
+  renderHeader() {
+    const { gravatarEmail, name, score } = this.props;
+
+    return (
+      <header>
+        <div className="header-left">
+          <div className="gravatar-image">
+            <img alt="gravatar" src={imageLink(gravatarEmail)} />
+          </div>
+          <p data-testid="header-player-name">{`Jogador: ${name}`}</p>
+        </div>
+        <div className="header-right">
+          <p data-testid="header-score">{`Pontos: ${score}`}</p>
+          <div className="config-button">
+            <button data-testid="config-button" />
+          </div>
+        </div>
+      </header>
+    );
+  }
+
+  renderSection() {
+    const { score, assertions} = this.props;
     const message = assertions < 3 ? 'Podia ser melhor...' : 'Mandou bem!';
+
+    return (
+      <section>
+        <h1 data-testid="feedback-text">{message}</h1>
+        <p data-testid="feedback-total-question">{`Você acertou ${assertions} questões!`}</p>
+        <p data-testid="feedback-total-score">{`Um total de ${score} pontos`}</p>
+        <div className="buttons">
+          <button
+            onClick={() => this.setState({ shouldRedirectToRanking: true })}
+            className="btn-ranking"
+          >
+            VER RANKING
+          </button>
+          <button
+            onClick={this.restartGame}
+            className="btn-play"
+          >
+            JOGAR NOVAMENTE
+          </button>
+        </div>
+      </section>
+    );
+  }
+
+  render() {
+    const { shouldRedirectToRanking, shouldRedirectToHome } = this.state;
 
     if (shouldRedirectToRanking) return <Redirect to="/ranking" />;
     if (shouldRedirectToHome) return <Redirect to="/" />;
 
     return (
       <div>
-        <header>
-          <div className="header-left">
-            <div className="gravatar-image">
-              <img alt="gravatar" src={imageLink(gravatarEmail)} />
-            </div>
-            <p data-testid="header-player-name">{`Jogador: ${name}`}</p>
-          </div>
-          <div className="header-right">
-            <p data-testid="header-score">{`Pontos: ${score}`}</p>
-            <div className="config-button">
-              <button data-testid="config-button" />
-            </div>
-          </div>
-        </header>
-        <section>
-          <h1 data-testid="feedback-text">{message}</h1>
-          <p data-testid="feedback-total-question">{`Você acertou ${assertions} questões!`}</p>
-          <p data-testid="feedback-total-score">{`Um total de ${score} pontos`}</p>
-          <div className="buttons">
-            <button
-              onClick={() => this.setState({ shouldRedirectToRanking: true })}
-              className="btn-ranking"
-            >
-              VER RANKING
-            </button>
-            <button onClick={this.restartGame} className="btn-play">JOGAR NOVAMENTE</button>
-          </div>
-        </section>
+        {this.renderHeader()}
+        {this.renderSection()}
       </div>
     );
   }
@@ -73,11 +93,11 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 Feedback.propTypes = {
-    name: propTypes.string.isRequired,
-    gravatarEmail: propTypes.string.isRequired,
-    score: propTypes.number.isRequired,
-    assertions: propTypes.number.isRequired,
-    clearPlayer: propTypes.func.isRequired,
+  name: propTypes.string.isRequired,
+  gravatarEmail: propTypes.string.isRequired,
+  score: propTypes.number.isRequired,
+  assertions: propTypes.number.isRequired,
+  clearPlayer: propTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Feedback);

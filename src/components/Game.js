6bc67faff5d14e4a9c0fc897509dd5bb4be04466
miguel-miranda <ayandram7727@gmail.com>
2020-04-02 06:22:05 -0300

    As alternativas ja sao dispostas aleatoriamente, independente do tipo. Ao clicar, a resposta correta fica verde e as incorretas vermelhas. Os botoes sao desabilitados ao clicar uma vez. O placar ainda NAO se altera, NEM se renderiza mais do que uma pergunta.

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,6 +4,15 @@ import imageLink from '../service/hashConverter';
 import './style/Game.css';
 
 class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foiRespondido: false,
+      arrayAlternativas: [],
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   // static renderHeader() {
   //   return (
   //     <header className="header-left">
@@ -63,19 +72,100 @@ class Game extends Component {
     );
   }
 
+  componentDidMount() {
+    const { dataMock } = this.props;
+    const primeiraPergunta = dataMock[0];
+    const { correct_answer, incorrect_answers } = primeiraPergunta;
+
+    const arrayAlternativas = [correct_answer];
+    let indexNovaAlternativa;
+
+    incorrect_answers.forEach((item, index) => {
+      indexNovaAlternativa = Math.round(Math.random() * (index + 1));
+      arrayAlternativas.splice(indexNovaAlternativa, 0, incorrect_answers[index]);
+    });
+    this.setState({ arrayAlternativas });
+  }
+
+  renderizaAPrimeiraPergunta() {
+    // const { dataMock } = this.props;
+    // const primeiraPergunta = dataMock[0];
+    // const { category, question, correct_answer, incorrect_answers } = primeiraPergunta;
+    // // const indexRespostaCorreta = Math.round(Math.random() * incorrect_answers.length);
+    // // const arrayAlternativas = [...incorrect_answers];
+    // // arrayAlternativas.splice(indexRespostaCorreta, 0, correct_answer);
+
+    // const arrayAlternativas = [correct_answer];
+    // let indexNovaAlternativa;
+
+    // incorrect_answers.forEach((item, index) => {
+    //   indexNovaAlternativa = Math.round(Math.random() * (index + 1));
+    //   arrayAlternativas.splice(indexNovaAlternativa, 0, incorrect_answers[index]);
+    // });
+    // let indexNovaAlternativa = Math.round(Math.random() * 1);
+    // arrayAlternativas.splice(indexNovaAlternativa, 0, incorrect_answers[0]);
+    // indexNovaAlternativa = Math.round(Math.random() * 2);
+    // arrayAlternativas.splice(indexNovaAlternativa, 0, incorrect_answers[1]);
+    // indexNovaAlternativa = Math.round(Math.random() * 3);
+    // arrayAlternativas.splice(indexNovaAlternativa, 0, incorrect_answers[2]);
+  
+    const { arrayAlternativas, foiRespondido } = this.state;
+    const { dataMock } = this.props;
+    const primeiraPergunta = dataMock[0];
+    const { category, question } = primeiraPergunta;
+
+    return (
+      <div>
+        <div className="question-box">
+          <h3>{category}</h3>
+          <p>{question}</p>
+        </div>
+        <div className="alternatives">
+          {arrayAlternativas.map((item) => (
+            <button
+              className={this.setaClasse(item)}
+              value={item}
+              onClick={this.handleClick}
+              disabled={foiRespondido}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  setaClasse(alternativa) {
+    const { foiRespondido } = this.state;
+    const { dataMock } = this.props;
+    const primeiraPergunta = dataMock[0];
+    const { correct_answer } = primeiraPergunta;
+    if (foiRespondido) {
+      return (alternativa === correct_answer) ? 'correct-answer' : 'incorrect-answer';
+    }
+    return 'alternative-button';
+  }
+
+  handleClick(event) {
+    this.setState({ foiRespondido: true });
+  }
+
   render() {
     return (
       <div>
         {this.renderHeader()}
+        {this.renderizaAPrimeiraPergunta()}
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ player: { gravatarEmail, name, score } }) => ({
+const mapStateToProps = ({ loadReducer: { dataMock }, player: { gravatarEmail, name, score } }) => ({
   gravatarEmail,
   name,
   score,
+  dataMock,
 });
 
 export default connect(mapStateToProps)(Game);
